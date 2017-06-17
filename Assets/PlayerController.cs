b2cd3a288ce10b1bab398f226fcffdc48c739ad2
John Hawley <jhawley@institute.local>
2017-06-16 23:33:02 -0500

    added transition from idle/running to falling in animator, refactored code

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,20 +7,23 @@ public class PlayerController : MonoBehaviour {
 	public float speed = 5.0f;
 	public float jumpForce = 200.0f;
 	public int jumpCountMax = 2;
-	private int jumpCount = 0;
+
 
 	Rigidbody2D body;
 	Animator anim;
+
 	List<GameObject> ground = new List<GameObject>();
-	float horizontal;
-	float vertical;
-	float lastVertical;
+	int jumpCount = 0;
+	float horizontalInput;
+	float verticalInput;
+	float previousVerticalInput;
+	float previousHorizontalInput;
+
 	public bool isGrounded {
 		get { return ground.Count > 0; }
 	}
-
-	public bool shouldJump {
-		get { return lastVertical < 0.5f && vertical >= 0.5f; }
+	public bool canJump {
+		get { return previousVerticalInput < 0.5f && verticalInput >= 0.5f; }
 	}
 
 	void Start(){
@@ -41,39 +44,34 @@ public class PlayerController : MonoBehaviour {
 
 
 	void GetAxis(){
-		horizontal = Input.GetAxisRaw("Horizontal");
-		lastVertical = vertical;
-		vertical = Input.GetAxisRaw ("Vertical");
+		horizontalInput = Input.GetAxisRaw("Horizontal");
+		previousVerticalInput = verticalInput;
+		verticalInput = Input.GetAxisRaw ("Vertical");
 	}
 
 	void FlipSprite(){
-		if (horizontal > 0) {
+		if (horizontalInput > 0) {
 			transform.right = Vector3.right;
 		} 
-		else if (horizontal < 0) {
+		else if (horizontalInput < 0) {
 			transform.right = -Vector3.right;
 		}
 	}
 
 	void SetVelocity(){
-		body.velocity = new Vector2(horizontal * speed, body.velocity.y);
+		body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-		if (shouldJump && (isGrounded || jumpCount < jumpCountMax)) {
+		if (canJump && (isGrounded || jumpCount < jumpCountMax)) {
 			jumpCount++;
 			body.velocity = new Vector2(body.velocity.x, jumpForce);
 		}
 	}
 		
 	void SetAnimations(){
-		anim.SetBool ("isRunning", Mathf.Abs (horizontal) > 0.1f);
+		anim.SetBool ("isRunning", Mathf.Abs (horizontalInput) > 0.1f);
 		anim.SetBool ("isJumping", body.velocity.y > 0.5f);
 		anim.SetBool ("isIdle", (isGrounded && body.velocity.magnitude < 0.1f));
 		anim.SetBool ("isGrounded", isGrounded);
-	}
-
-	void SetAttributes(){
-		if (isGrounded)
-			jumpCount = 2;
 	}
 		
 

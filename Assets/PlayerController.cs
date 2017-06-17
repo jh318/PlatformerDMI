39f016a6cadf3495a6ffa8b7eb0ba4bbe47b048e
John Hawley <jhawley@institute.local>
2017-06-16 22:14:52 -0500

    added ground check, got animation transitions working for jumping

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 5.0f;
+	public float jumpForce = 200.0f;
 
 	Rigidbody2D body;
 	Animator anim;
+	float horizontal;
+	float vertical;
+	List<GameObject> ground = new List<GameObject>();
+
+	public bool isGrounded {
+		get { return ground.Count > 0; }
+	}
 
 	void Start(){
 		body = GetComponent<Rigidbody2D> ();
@@ -15,18 +23,70 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void Update(){
-		float horizontal = Input.GetAxisRaw("Horizontal");
-		float vertical = Input.GetAxisRaw ("Vertical");
+		GetAxis ();
+		FlipSprite ();
+		SetVelocity ();
+		SetAnimations ();
+		CollisionCheck ();
 
-		if (horizontal > 0) {
-			transform.right = Vector3.right;
-		} else if (horizontal < 0) {
-			transform.right = -Vector3.right;
-		}
 
-		body.velocity = new Vector2(horizontal, body.velocity.y) * speed;
 
-		anim.SetBool ("isRunning", Mathf.Abs (horizontal) > 0.1f);
+
 	}
 
+
+	void GetAxis(){
+		horizontal = Input.GetAxisRaw("Horizontal");
+		vertical = Input.GetAxisRaw ("Vertical");
+	}
+
+	void FlipSprite(){
+		if (horizontal > 0) {
+			transform.right = Vector3.right;
+		} 
+		else if (horizontal < 0) {
+			transform.right = -Vector3.right;
+		}
+	}
+
+	void SetVelocity(){
+		body.velocity = new Vector2(horizontal * speed, body.velocity.y);
+
+		if (vertical > 0.1f && isGrounded) {
+			body.AddForce (transform.up * jumpForce, ForceMode2D.Impulse);
+		}
+	}
+		
+	void SetAnimations(){
+		anim.SetBool ("isRunning", Mathf.Abs (horizontal) > 0.1f);
+		anim.SetBool ("isJumping", (!isGrounded));
+		anim.SetBool ("isIdle", (isGrounded && body.velocity == new Vector2 (0, 0)));
+		//if (isGrounded && !anim.GetBool("isJumping")) {
+		//	anim.SetBool ("isLanding", true);
+		//}
+		//anim.SetBool("is", )
+	}
+
+	void CollisionCheck(){
+		
+	}
+
+
+	///COLLISION
+	//Check for Ground
+	void OnCollisionEnter2D(Collision2D c){
+		foreach (ContactPoint2D cp in c.contacts) {
+			if (Vector2.Angle (cp.normal, Vector2.up) < 45) {
+				ground.Add(c.gameObject);
+				return;
+			}
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D c) {
+		if (ground.Contains(c.gameObject)) {
+			ground.Remove(c.gameObject);
+		}
+	}
 }
+

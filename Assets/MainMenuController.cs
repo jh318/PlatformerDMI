8c613441d2ed85e1f8b18173ac5661c66211b220
John Hawley <jhawley@institute.local>
2017-07-07 22:00:30 -0500

    edited for turn in build

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour {
 
 	public string sceneToLoad;
 
+
+	void Start(){
+		SceneManager.LoadScene("GameManager", LoadSceneMode.Additive);
+	}
 	void Update(){
 		if(Input.GetButtonDown("SlashButton")){
 			GameManager.instance.GetComponent<SceneLoader>().LoadScene(sceneToLoad);

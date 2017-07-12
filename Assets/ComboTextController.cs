3cf539dd803b38bf49831c85dcf51a81336ae7dd
John Hawley <jhawley@institute.local>
2017-07-11 23:36:15 -0500

    added longest air time tracker and ranking tracker. added ranks

---
+++ Assets/ComboTextController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComboTextController : MonoBehaviour {
+
+	public static ComboTextController instance;
+
+	float airTimer = 0.0f;
+	Text comboText;
+
+	float longestAirTime = 0.0f;
+	string highestRating = "";
+
+	public float LongestAirTime{
+		get { return longestAirTime; }
+	}
+	public string HighestRating{
+		get{ return highestRating; }
+	}
+
+	void Awake(){
+		if(instance == null){
+			instance = this;
+		}
+	}
+
+	void Start () {
+		comboText = GetComponent<Text>();
+		highestRating = "Grounded!!!";
+	}
+	
+	void Update () {
+		if(!PlayerController.instance.isGrounded){
+			airTimer += Time.deltaTime;
+			//comboText.text = airTimer.ToString("F3");
+		}
+		if(PlayerController.instance.isGrounded){
+			airTimer = 0.0f;
+			comboText.text = "Grounded!!!";
+			//comboText.text = airTimer.ToString("F3");
+		}
+		ComboCommentary();
+		Ranker();
+		Debug.Log(highestRating);
+	}
+
+	void ComboCommentary(){
+		if(airTimer > 5.0f) comboText.text = "Dood!";
+		if(airTimer > 10.0f) comboText.text = "Crazy!";
+		if(airTimer > 15.0f) comboText.text = "Barrel Rollin'!";
+		if(airTimer > 20.0f) comboText.text = "ACE";
+		if(airTimer > 25.0f) comboText.text = "SUPA'!";
+		if(airTimer > 30.0f) comboText.text = "SUPA' FLYYYY!";
+		if(airTimer > 35.0f) comboText.text = "SUPA' FLY STYLIN' ";
+	}
+
+	void Ranker(){
+		if(airTimer > longestAirTime) longestAirTime = airTimer;
+		if(longestAirTime >= 5.0f) highestRating = "Dood!";
+		if(longestAirTime >= 10.0f) highestRating = "Crazy!";
+		if(longestAirTime >= 15.0f) highestRating = "Barrel Rollin'!";
+		if(longestAirTime >= 20.0f) highestRating = "ACE";
+		if(longestAirTime >= 25.0f) highestRating = "SUPA'!";
+		if(longestAirTime >= 30.0f) highestRating = "SUPA' FLYYYY!";
+		if(longestAirTime >= 35.0f) highestRating = "SUPA' FLY STYLIN'";
+	}
+}
